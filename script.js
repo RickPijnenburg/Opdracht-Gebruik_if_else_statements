7bb884166151a3f01f8a14d42da7fe49eda062bb
RickPijnenburg <32143619+RickPijnenburg@users.noreply.github.com>
2021-09-18 21:29:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const age = 18;
 const isFemale = true;
-const driverStatus = "bob"
+const driverStatus = "bob";
 
 if (age >= 18) {
-    console.log("Persoon mag naar binnen!")
+    console.log("Persoon mag naar binnen!");
 } else {
-    console.log("Persoon mag niet naar binnen!")
+    console.log("Persoon mag niet naar binnen!");
 }
 
 if (isFemale){
-    console.log("Persoon is een vrouw en mag naar binnen!")
+    console.log("Persoon is een vrouw en mag naar binnen!");
 } else {
-    console.log("Persoon is geen vrouw en mag niet naar binnen!")
+    console.log("Persoon is geen vrouw en mag niet naar binnen!");
 }
 
 if(driverStatus === 'bob'){
-    console.log("Persoon mag naar huis rijden!")
+    console.log("Persoon mag naar huis rijden!");
 } else {
-    console.log("Persoon mag niet naar huis rijden!")
+    console.log("Persoon mag niet naar huis rijden!");
 }
